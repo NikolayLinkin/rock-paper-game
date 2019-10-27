66228d09ecfd8d49abb048b2d5b6a336bd826699
Nikolay Linkin <kolera941@gmail.com>
2019-10-27 22:00:18 +0300

    Updates

---
+++ precache-manifest.52982b28699eb6988180c049c46e0553.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81ad6ec614b51b0fbf274bdb004922e0",
+    "url": "/rock-paper-game/index.html"
+  },
+  {
+    "revision": "63255015b69626e5cbbe",
+    "url": "/rock-paper-game/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "65fd9da9fcbd1a2186ec",
+    "url": "/rock-paper-game/static/css/main.de93a109.chunk.css"
+  },
+  {
+    "revision": "63255015b69626e5cbbe",
+    "url": "/rock-paper-game/static/js/2.a88dac61.chunk.js"
+  },
+  {
+    "revision": "65fd9da9fcbd1a2186ec",
+    "url": "/rock-paper-game/static/js/main.3c5137aa.chunk.js"
+  },
+  {
+    "revision": "a6d2ca8680113057b765",
+    "url": "/rock-paper-game/static/js/runtime-main.9e37a4cb.js"
+  },
+  {
+    "revision": "1d2621e5852646cb449ff778ff014969",
+    "url": "/rock-paper-game/static/media/ico-paper.1d2621e5.svg"
+  },
+  {
+    "revision": "a6b80a0effe992a7dab3a0da18335d39",
+    "url": "/rock-paper-game/static/media/ico-rock.a6b80a0e.svg"
+  },
+  {
+    "revision": "d24e4bd5f355ec0bdf176bd36d33678a",
+    "url": "/rock-paper-game/static/media/ico-scissors.d24e4bd5.svg"
+  }
+]);
